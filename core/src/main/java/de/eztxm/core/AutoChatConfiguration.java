875254bdf7bc82dcf00c8ddd3032b16d2206af34
ezTxmMC <eztxmmc@hotmail.com>
2024-11-09 22:10:35 +0100

    1.0.0-BETA3

--- core/src/main/java/de/eztxm/core/AutoChatConfiguration.java
+++ core/src/main/java/de/eztxm/core/AutoChatConfiguration.java
@@ -1,11 +1,13 @@
 package de.eztxm.core;
 
 import net.labymod.api.addon.AddonConfig;
-import net.labymod.api.client.gui.screen.widget.widgets.input.AdvancedSelectionWidget.AdvancedSelectionSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget.TextFieldSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
+import net.labymod.api.configuration.loader.annotation.Exclude;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
+import java.util.HashMap;
+import java.util.Map;
 
 @ConfigName("settings")
 public class AutoChatConfiguration extends AddonConfig {
@@ -16,8 +18,8 @@ public class AutoChatConfiguration extends AddonConfig {
   private final ConfigProperty<String> server = new ConfigProperty<>("");
   @TextFieldSetting
   private final ConfigProperty<String> input = new ConfigProperty<>("");
-  @AdvancedSelectionSetting
-  private final ConfigProperty<String> advancedSelection = new ConfigProperty<>("Hi");
+  @Exclude
+  private Map<String, String> serverAndInput = new HashMap<>();
 
   @Override
   public ConfigProperty<Boolean> enabled() {
@@ -30,5 +32,9 @@ public class AutoChatConfiguration extends AddonConfig {
 
   public ConfigProperty<String> input() {
     return this.input;
+  }
+
+  public Map<String, String> serverAndInput() {
+    return this.serverAndInput;
   }
 }
